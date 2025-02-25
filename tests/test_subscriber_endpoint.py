251b13a6dbfae1e8bc06eaa951b4616c05caf01e
sebastianknopf <mail@sebastian-knopf.de>
2025-02-25 10:02:28 +0100

    added test cases for subscriber and publisher endpoints, references #4

---
+++ tests/test_subscriber_endpoint.py
@@ -0,0 +1,57 @@
+import os
+import unittest
+
+from fastapi.testclient import TestClient
+
+from vdv736.subscriber import SubscriberEndpoint
+from vdv736.response import xml2siri_response
+from vdv736.sirixml import get_value as sirixml_get_value
+
+class SubscriberEndpoint_Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.endpoint = SubscriberEndpoint('TEST')
+
+        cls.client = TestClient(cls.endpoint.create_endpoint(
+            'TEST', 
+            '/vdv736'
+        ))
+
+        return super().setUpClass()
+    
+    def test_SampleServiceDelivery(self):
+        xml_filename = os.path.join(os.path.dirname(__file__), 'data/xml/SampleServiceDelivery.xml')
+        with open(xml_filename, 'r') as xml_file:
+            response = self.client.post('/vdv736', content=xml_file.read())
+
+            self.assertEqual(200, response.status_code)
+
+            siri_response = xml2siri_response(response.content)
+            self.assertEqual(True, siri_response.Siri.DataReceivedAcknowledgement.Status)
+
+    def test_InvalidXmlData(self):
+        xml_filename = os.path.join(os.path.dirname(__file__), 'data/xml/InvalidXmlData.xml')
+        with open(xml_filename, 'r') as xml_file:
+            response = self.client.post('/vdv736', content=xml_file.read())
+
+            self.assertEqual(400, response.status_code)
+
+    def test_InvalidEndpoint(self):
+        response = self.client.post('/someInvalidEndpoint')
+        self.assertEqual(404, response.status_code)
+
+    def test_InvalidMethod(self):
+        response = self.client.get('/vdv736')
+        self.assertEqual(405, response.status_code)
+
+    @classmethod
+    def tearDownClass(cls):
+
+        # close local node database in order to cleanup endpoint data
+        # note: normally, this would be done by the Subscriber class, as the endpoint
+        # is started as thread of the subscriber!
+        cls.endpoint._local_node_database.close(True)
+
+        return super().tearDownClass()
+            
