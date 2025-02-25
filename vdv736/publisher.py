c188f75c3e40d856f51542514dad16001a64e2c4
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-02-25 16:52:46 +0100

    added parameter for local IP address in Subscriber and Publisher constructor, references #13

--- vdv736/publisher.py
+++ vdv736/publisher.py
@@ -28,8 +28,10 @@ from threading import Thread
 
 class Publisher():
 
-    def __init__(self, participant_ref: str, participant_config_filename: str):
+    def __init__(self, participant_ref: str, participant_config_filename: str, local_ip_address: str = '0.0.0.0'):
         self._service_participant_ref = participant_ref
+        self._service_local_ip_address = local_ip_address
+
         self._logger = logging.getLogger('uvicorn')
 
         self._local_node_database = local_node_database('vdv736.publisher')
@@ -89,7 +91,7 @@ class Publisher():
         logging.getLogger('uvicorn.asgi').propagate = False
 
         # run ASGI server with endpoint
-        endpoint_host = self._participant_config.participants[self._service_participant_ref]['host']
+        endpoint_host = self._service_local_ip_address # self._participant_config.participants[self._service_participant_ref]['host']
         endpoint_port = self._participant_config.participants[self._service_participant_ref]['port']
 
         uvicorn.run(app=self._endpoint.create_endpoint(
