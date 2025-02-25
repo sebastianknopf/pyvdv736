86f76be893f8e7e0f7a6ca5dcf1ae5ec8c48c894
sebastianknopf <mail@sebastian-knopf.de>
2025-02-25 12:14:19 +0100

    intended method add_or_update_situations(), references #10

--- vdv736/subscriber.py
+++ vdv736/subscriber.py
@@ -294,7 +294,7 @@ class SubscriberEndpoint():
             # process service delivery ...
             for pts in sirixml_get_elements(delivery, 'Siri.ServiceDelivery.SituationExchangeDelivery.Situations.PtSituationElement'):
                 situation_id = sirixml_get_value(pts, 'SituationNumber')
-                self._local_node_database.add_situation(situation_id, pts)
+                self._local_node_database.add_or_update_situation(situation_id, pts)
 
             # create data acknowledgement with OK status
             acknowledgement = DataReceivedAcknowledgement(
