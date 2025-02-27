c3c4e780b230bee94d0d6bcf0c40e9da4f3c8473
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2025-02-27 11:49:35 +0100

    fixed some bugs in demo_subscriber.py, encanced logging for deliveries

--- demo/demo_subscriber.py
+++ demo/demo_subscriber.py
@@ -4,16 +4,26 @@ import time
 
 from vdv736.subscriber import Subscriber
 from vdv736.delivery import SiriDelivery
+from vdv736.sirixml import exists as sirixml_exists
+from vdv736.sirixml import get_elements as sirixml_get_elements
 
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
 
 def on_delivery(delivery: SiriDelivery) -> None:
     print('Delivery callback called...')
 
+    if sirixml_exists(delivery, 'Siri.ServiceDelivery.SituationExchangeDelivery.Situations'):
+        pt_situations = sirixml_get_elements(delivery, 'Siri.ServiceDelivery.SituationExchangeDelivery.Situations.PtSituationElement')
+        print(f"Found {len(pt_situations)} situations in this delivery")
+
+
+
 with Subscriber('PY_TEST_SUBSCRIBER', './demo/demo_participants.yaml') as subscriber:
 
     if len(sys.argv) > 1 and sys.argv[1] == 'ps':
-    
+
+        subscriber.set_callbacks(on_delivery)
+
         time.sleep(15)
         sid = subscriber.subscribe('PY_TEST_PUBLISHER')
         time.sleep(10)
@@ -21,7 +31,6 @@ with Subscriber('PY_TEST_SUBSCRIBER', './demo/demo_participants.yaml') as subscr
         time.sleep(10)
         subscriber.unsubscribe(sid)
 
-        subscriber.set_callbacks(on_delivery)
         sid = subscriber.subscribe('PY_TEST_PUBLISHER')
 
         while True:
