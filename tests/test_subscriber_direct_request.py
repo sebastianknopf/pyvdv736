9bd9113d8eacf70089c837455403db68187624e3
sebastianknopf <mail@sebastian-knopf.de>
2025-03-27 16:08:31 +0100

    added basic frame for test case for request/response in subscriber, references #25

---
+++ tests/test_subscriber_direct_request.py
@@ -0,0 +1,16 @@
+import os
+import unittest
+import unittest.mock
+
+class SubscriberDirectRequest_Test(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_DirectRequestResponse(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        return super().tearDownClass()
