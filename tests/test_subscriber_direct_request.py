fda735fa5957d87c3d23f20b6e3f9625e46f2c90
sebastianknopf <mail@sebastian-knopf.de>
2025-03-28 08:09:44 +0100

    fixed test cases using responses package, references #25

--- tests/test_subscriber_direct_request.py
+++ tests/test_subscriber_direct_request.py
@@ -1,69 +1,48 @@
-import http.server
-import logging
 import os
-import socketserver
-import threading
+import responses
 import unittest
 import unittest.mock
 
 from vdv736.subscriber import Subscriber
 
-
-class MockHttpPublisherHandler(http.server.SimpleHTTPRequestHandler):
-    def do_POST(self):
-        if self.path == '/request':
-            self.send_response(200)
-            self.send_header("Content-type", "application/xml")
-            self.end_headers()
-
-            xml_filename = os.path.join(os.path.dirname(__file__), 'data/xml/SampleServiceDelivery.xml')
-            with open(xml_filename, 'rb') as xml_file:
-                xml_content = xml_file.read()
-
-            self.wfile.write(xml_content)
-        else:
-            self.send_response(200)
-
-    def log_message(self, format, *args):
-        pass
-
-
 class SubscriberDirectRequest_Test(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.server = socketserver.TCPServer(("localhost", 9091), MockHttpPublisherHandler)
-        cls.thread = threading.Thread(target=cls.server.serve_forever)
-        cls.thread.daemon = True
-        cls.thread.start()
+    def setUp(self):
+        xml_filename = os.path.join(os.path.dirname(__file__), 'data/xml/SampleServiceDelivery.xml')
+        with open(xml_filename, 'r') as xml_file:
+            xml_content = xml_file.read()
+        
+        self.responder = responses.RequestsMock()
+        self.responder.start()
 
-        logging.disable(logging.CRITICAL)
-
-        configfile = os.path.join(os.path.dirname(__file__), 'data/yaml/participants_testconfig.yaml')
-        cls.subscriber = Subscriber('PY_TEST_SUBSCRIBER', configfile)
-        cls.subscriber.__enter__()
+        self.responder.add(
+            responses.POST,
+            'http://127.0.0.1:9091/request',
+            body=xml_content,
+            content_type='application/xml',
+            status=200
+        )
 
     def test_DirectRequestResponse(self):
-        self.assertEqual(True, self.subscriber.request('PY_TEST_PUBLISHER'))
-        self.assertEqual(1, len(self.subscriber.get_situations()))
+
+        configfile = os.path.join(os.path.dirname(__file__), 'data/yaml/participants_testconfig.yaml')        
+        with Subscriber('PY_TEST_SUBSCRIBER', configfile, publish_subscribe=False) as subscriber:
+            self.assertEqual(True, subscriber.request('PY_TEST_PUBLISHER'))
+            self.assertEqual(1, len(subscriber.get_situations()))
 
     def test_DirectRequestResponseWithCallback(self):
+  
         on_delivery_callback = unittest.mock.Mock()
         
-        self.subscriber.set_callbacks(on_delivery_callback)
+        configfile = os.path.join(os.path.dirname(__file__), 'data/yaml/participants_testconfig.yaml')
+        with Subscriber('PY_TEST_SUBSCRIBER', configfile, publish_subscribe=False) as subscriber:
+            subscriber.set_callbacks(on_delivery_callback)
 
-        self.assertEqual(True, self.subscriber.request('PY_TEST_PUBLISHER'))
-        on_delivery_callback.assert_called()
+            self.assertEqual(True, subscriber.request('PY_TEST_PUBLISHER'))
+            on_delivery_callback.assert_called()
             
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.server.shutdown()
-        cls.server.server_close()
-        cls.thread.join()
-
-        logging.disable(logging.NOTSET)
-
-        cls.subscriber.__exit__(None, None, None)
+    def tearDown(self):
+        self.responder.stop()
+        self.responder.reset()
 
         return super().tearDownClass()
