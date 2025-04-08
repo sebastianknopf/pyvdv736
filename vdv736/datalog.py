2683aacf19817bef24a6ca7fbab2f7334fbb9d96
sebastianknopf <mail@sebastian-knopf.de>
2025-04-08 15:08:01 +0200

    hotfix in datalog.py, fixed broken reference in datetime call

--- vdv736/datalog.py
+++ vdv736/datalog.py
@@ -20,7 +20,7 @@ class Datalog:
             # proceed only if the datalogfile is not from today
             if not datalog_file.startswith(today):
                 datalog_timestamp = datalog_file.split('_')[0]
-                datalog_timestamp = datetime.datetime.strptime(datalog_timestamp, '%Y-%m-%d-%H.%M.%S-%f')
+                datalog_timestamp = datetime.strptime(datalog_timestamp, '%Y-%m-%d-%H.%M.%S-%f')
 
                 difference = (datetime.datetime.now() - datalog_timestamp).total_seconds()
                 if difference > 60 * 60 * ttl_hours:
