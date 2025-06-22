be6ea36c230f317305f645f9620f952c63ede639
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-06-22 23:22:00 +0200

    improved test case for handling a disappearing situation properly in direct request mode, references #40

--- tests/test_subscriber_direct_request.py
+++ tests/test_subscriber_direct_request.py
@@ -5,6 +5,7 @@ import unittest.mock
 
 from vdv736.subscriber import Subscriber
 from vdv736.request import InvalidMethodError
+from vdv736.sirixml import get_value as sirixml_get_value
 
 class SubscriberDirectRequest_Test(unittest.TestCase):
 
@@ -44,6 +45,27 @@ class SubscriberDirectRequest_Test(unittest.TestCase):
             status=200
         )
 
+        # for test_DirectRequestWithDisappearingSituation
+        cls.responder.add(
+            responses.POST,
+            'http://127.0.0.1:9091/request',
+            body=xml_content,
+            content_type='application/xml',
+            status=200
+        )
+
+        xml_filename = os.path.join(os.path.dirname(__file__), 'data/xml/SampleEmptyServiceDelivery.xml')
+        with open(xml_filename, 'r') as xml_file:
+            xml_content = xml_file.read()
+
+        cls.responder.add(
+            responses.POST,
+            'http://127.0.0.1:9091/request',
+            body=xml_content,
+            content_type='application/xml',
+            status=200
+        )
+
     def test_DirectRequestResponse(self):
 
         configfile = os.path.join(os.path.dirname(__file__), 'data/yaml/participants_testconfig.yaml')        
@@ -69,6 +91,17 @@ class SubscriberDirectRequest_Test(unittest.TestCase):
             self.assertEqual(True, subscriber.request('PY_TEST_PUBLISHER', method='GET'))
             self.assertEqual(1, len(subscriber.get_situations()))
 
+    def test_DirectRequestWithDisappearingSituation(self):
+
+        configfile = os.path.join(os.path.dirname(__file__), 'data/yaml/participants_testconfig.yaml')        
+        with Subscriber('PY_TEST_SUBSCRIBER', configfile, publish_subscribe=False) as subscriber:
+            self.assertEqual(True, subscriber.request('PY_TEST_PUBLISHER'))
+            self.assertEqual(1, len(subscriber.get_situations()))
+            self.assertEqual('ef478576-d1a8-527e-8820-5164ca986128', sirixml_get_value(next(iter(subscriber.get_situations().values())), 'SituationNumber', None))
+
+            self.assertEqual(True, subscriber.request('PY_TEST_PUBLISHER'))
+            self.assertEqual(0, len(subscriber.get_situations()))
+                    
     def test_DirectRequestResponseUsingInvalidMethod(self):
 
         configfile = os.path.join(os.path.dirname(__file__), 'data/yaml/participants_testconfig.yaml')        
